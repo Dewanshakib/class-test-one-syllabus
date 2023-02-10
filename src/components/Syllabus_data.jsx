0df9f801b58cb0e4d390fbc0f67850f3a402c54b
Dewan Shakib <shakibrocks2042@gmail.com>
2023-02-10 19:28:35 +0600

    update 3

--- src/components/Syllabus_data.jsx
+++ src/components/Syllabus_data.jsx
@@ -2,21 +2,21 @@ export const syllabus_Data = [
     {
         id:1,
         title:"Physics Syllabus",
-        img:"Physics Syllabus.png",
+        img:"Physics Syllabus.jpg",
         test:"Class Test One",
         down:"Download",
     },
     {
         id:2,
         title:"Chemistry Syllabus",
-        img:"Chemistry Syllabus.png",
+        img:"Chemistry Syllabus.jpg",
         test:"Class Test One",
         down:"Download",
     },
     {
         id:3,
         title:"ICT Syllabus",
-        img:"ICT Syllabus.png",
+        img:"ICT Syllabus.jpg",
         test:"Class Test One",
         down:"Download",
     }
